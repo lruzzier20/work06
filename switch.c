dbe3c3b749a774743ee3f48e5fb8b00c3daceeea
Logan Ruzzier <lruzzier@DESKTOP-MQGMU3Q.localdomain>
2021-10-03 16:50:52 -0400

    finished 1-4

--- switch.c
+++ switch.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 int main(){
   int arr[10];
+  arr[0]=0;
+  srand(time(NULL));
+  for(int i=1;i<10;i++){
+    arr[i]=rand();
+  }
+  for(int j=0;j<10;j++){
+    printf("arr[%d]=%d\n",j,arr[j]);
+  }
 }

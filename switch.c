370933659173a4b395b51cc79720649bd9b0297e
Logan Ruzzier <lruzzier@DESKTOP-MQGMU3Q.localdomain>
2021-10-03 17:00:12 -0400

    completed homework

--- switch.c
+++ switch.c
@@ -11,4 +11,14 @@ int main(){
   for(int j=0;j<10;j++){
     printf("arr[%d]=%d\n",j,arr[j]);
   }
+  int arr2[10];
+  int* ap=&arr;
+  int* ap2=&arr2;
+  printf("-------------------\n");
+  for(int k=0;k<10;k++){
+    ap2[k]=ap[9-k];
+  }
+  for(int l=0;l<10;l++){
+    printf("arr2[%d]=%d\n",l,*(ap2+l));
+  }
 }
